8b75da45b1cbfcf5ac74868902fd9570cbb4c441
ilosrim <ilosrim@yahoo.com>
2022-01-09 00:02:23 +0500

    progress bar add number progress

--- 01-progress-bar/app.js
+++ 01-progress-bar/app.js
@@ -1,4 +1,27 @@
-const progress = document.querySelector(".progress-done");
+// const progress = document.querySelector(".progress-done");
 
-progress.style.opacity = 1;
-progress.style.width = progress.getAttribute('data-done') + '%';
+// progress.style.opacity = 1;
+// progress.style.width = progress.getAttribute('data-done') + '%';
+
+var i = 0;
+
+function move() {
+  if (i == 0) {
+    i = 1;
+    var progress = document.querySelector(".progress-done");
+    var width = 0;
+    var id = setInterval(frame, 20);
+
+    function frame() {
+      if (width >= 70) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        progress.style.width = width + '%';
+        progress.style.opacity = 1;
+        progress.innerHTML = width + '%'
+      }
+    }
+  }
+}
