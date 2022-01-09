f9746917674a4a606203f926ee73f1c579c1bcfc
ilosrim <ilosrim@yahoo.com>
2022-01-09 23:12:21 +0500

    Line Through Effect

---
+++ 04-line-through/app.js
@@ -0,0 +1,18 @@
+const text = document.getElementById("text");
+const textArr = text.innerText.split('');
+
+const newEl = document.createElement('h1');
+
+newEl.innerHTML = `
+  ${textArr.map(letter=>`<span style="${randomVisibility()}">${letter}</span>`).join('')}
+`;
+
+newEl.classList.add('overlay');
+
+document.body.appendChild(newEl);
+
+console.log(randomVisibility());
+
+function randomVisibility() {
+  return Math.random > 0.5 ? 'visibility:hidden' : 'visibility:visible';
+}
