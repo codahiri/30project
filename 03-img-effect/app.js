b20db7870c3fb3bd5f0f074bd9aac5c0a45c77e0
ilosrim <ilosrim@yahoo.com>
2022-01-09 21:16:52 +0500

    Image Password Strength Effect

---
+++ 03-img-effect/app.js
@@ -0,0 +1,9 @@
+const password = document.getElementById("password");
+const backgraund = document.getElementById("backgraund");
+
+password.addEventListener('input', (e) => {
+  const input = e.target.value;
+  const length = input.length;
+  const blurValue = 20 - length * 2;
+  backgraund.style.filter = `blur(${blurValue}px)`;
+})
