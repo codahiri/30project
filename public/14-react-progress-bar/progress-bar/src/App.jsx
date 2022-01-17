03fb7b894f51100e2a5002eb71cc0010f3729673
ilosrim <ilosrim@yahoo.com>
2022-01-17 16:59:36 +0500

    CSS Smooth Scroll

---
+++ public/14-react-progress-bar/progress-bar/src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import './App.css'
+
+const Progress = ({ done }) => {
+
+  const [style, setStyle] = useState({});
+
+  setTimeout(() => {
+    const newStyle = {
+      opacity: 1,
+      width: `${done}%`
+    }
+    setStyle(newStyle)
+  }, 1000)
+
+  return (
+    <div class="progress">
+      <div class="progress-done" style={style}>
+        {done}%
+      </div>
+    </div>
+  )
+}
+
+function App() {
+
+
+  return (
+    <>
+      <h1>React Progress Bar</h1>
+      <Progress done="70" />
+    </>
+  )
+}
+
+export default App
